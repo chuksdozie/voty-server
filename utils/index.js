1bed467a76ca15e0ff822119054bbfb1c7d96524
chuks dozie <chuksdozie48@gmail.com>
2022-08-03 02:18:41 +0100

    initial voter Id function

--- utils/index.js
+++ utils/index.js
@@ -2,6 +2,15 @@ const now = () => {
   return new Date().toISOString();
 };
 
+const generateVoterId = () => {
+  const data = "45qwga76";
+  console.log(data);
+  return data;
+};
+
+generateVoterId();
+
 module.exports = {
   now,
+  generateVoterId,
 };
